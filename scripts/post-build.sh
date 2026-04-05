b134e6973b70715e04a4354d06e7c9718a7cd3e7
bayu <bayu@jrengmusic.com>
2026-04-05 13:00:08 +0700

    v0.0.1

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # Called by goreleaser after each build. Signs and notarizes macOS binaries.
 # Env vars set by goreleaser: HOOK_TARGET (e.g. darwin_arm64), HOOK_PATH (binary path)
 
@@ -12,11 +14,12 @@ case "$HOOK_TARGET" in
     codesign --verify --verbose "$HOOK_PATH"
 
     echo "Notarizing $HOOK_PATH..."
-    ZIP=$(mktemp /tmp/cake-notarize-XXXXXX.zip)
+    ZIPDIR=$(mktemp -d)
+    ZIP="$ZIPDIR/$(basename "$HOOK_PATH").zip"
     ditto -c -k --keepParent "$HOOK_PATH" "$ZIP"
     xcrun notarytool submit "$ZIP" \
       --keychain-profile notary \
       --wait
-    rm "$ZIP"
+    rm -rf "$ZIPDIR"
     ;;
 esac
